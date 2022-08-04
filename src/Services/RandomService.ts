b5b62b249c0dc870fd037b8f18cc6fc3d4031872
TheWizardo <oz.sabbag@gmail.com>
2022-08-05 01:36:29 +0300

    getting random elements from array

--- src/Services/RandomService.ts
+++ src/Services/RandomService.ts
@@ -1,7 +1,35 @@
+import { Type } from "typescript";
+
 class RandomService {
-    public randomNumberFromArray(arr: boolean[]) {
+    public randomInt(max: number, min = 0): number {
+        let true_max = max;
+        let true_min = min;
+        if (max < min) {
+            true_max = min;
+            true_min = max;
+        }
+        return Math.floor(Math.random() * (true_max - true_min)) + true_min;
+    }
+
+    public randomNumberFromArray(arr: boolean[]): number {
         const indexes = arr.map((e, i) => e ? -1 : i).filter(e => e !== -1);
-        return indexes[Math.floor(Math.random() * indexes.length)];
+        return indexes[this.randomInt(indexes.length)];
+    }
+
+    public randomElementsFromArray<T>(arr: T[], amount: number): T[] {
+        if (amount >= arr.length) {
+            return arr;
+        }
+        const rand_arr: T[] = [];
+        let index = this.randomInt(arr.length);
+        while (rand_arr.length < 4) {
+            if (!rand_arr.includes(arr[index]))
+            {
+                rand_arr.push(arr[index]);
+            }
+            index = this.randomInt(arr.length);
+        }
+        return rand_arr;
     }
 }
 
