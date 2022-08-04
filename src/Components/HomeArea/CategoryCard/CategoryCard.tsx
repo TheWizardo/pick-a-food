ac77db1cd29e2352abf0e64a1014be76dfc4313b
TheWizardo <oz.sabbag@gmail.com>
2022-08-04 21:28:05 +0300

    card for display

---
+++ src/Components/HomeArea/CategoryCard/CategoryCard.tsx
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import "./CategoryCard.css";
+
+interface CategoryProps {
+    name: string;
+    imgPath: string;
+}
+
+
+
+function CategoryCard(category: CategoryProps): JSX.Element {
+    return (
+        <div className="CategoryCard">
+            <h1>{category?.name}</h1>
+            <img src={category?.imgPath}/>
+        </div>
+    );
+}
+
+export default CategoryCard;
