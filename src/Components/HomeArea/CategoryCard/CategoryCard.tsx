b3aa1636e22ba85a0e3104055a1d25cd4ad2e070
TheWizardo <oz.sabbag@gmail.com>
2022-08-05 01:35:05 +0300

    cosmetic

--- src/Components/HomeArea/CategoryCard/CategoryCard.tsx
+++ src/Components/HomeArea/CategoryCard/CategoryCard.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import "./CategoryCard.css";
 
 interface CategoryProps {
@@ -6,13 +5,11 @@ interface CategoryProps {
     imgPath: string;
 }
 
-
-
 function CategoryCard(category: CategoryProps): JSX.Element {
     return (
         <div className="CategoryCard">
-            <h1>{category?.name}</h1>
-            <img src={category?.imgPath}/>
+            <h1>{category.name}</h1>
+            <img src={category.imgPath}/>
         </div>
     );
 }
